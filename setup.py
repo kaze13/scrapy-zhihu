1689d435d0da22c4645ad9dc72c1e8861fb64b77
fc <feng_c@worksap.co.jp>
2014-11-25 10:22:16 +0800

    add setup info

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name='scrapy-mongo-zhihu',
+	version='0.1',
+	description='',
+	author='',
+	url='',
+	author_email='',
+	py_modules=['zhihu']
+)
